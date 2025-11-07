6cc3a8d731a9bc464fde02572fdf7d56e0927b0b
Centralbobcat54 <Killportes@hotmail.com>
2025-11-07 18:43:58 -0300

    Add files via upload
    Tarefa para o dia 09/11

---
+++ README.txt
@@ -0,0 +1,6 @@
+Web v2.1 Interface Guide Module
+Includes:
+- TarefaWebController.java
+- index.html with htmx form
+- fragments.html row fragment
+Add Thymeleaf dependency to pom.xml and integrate into your Spring project.
---
+++ TarefaTests.java
@@ -0,0 +1,12 @@
+package br.com.curso.listadetarefas.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
